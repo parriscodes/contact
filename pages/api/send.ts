a1eee03af40981d4036ca6557234a5f7bc42f9be
parriscodes <106174032+parriscodes@users.noreply.github.com>
2023-04-26 20:21:47 -0400

    Update send.ts

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -15,7 +15,7 @@ export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
       message,
     }: { name: string; email: string; message: string } = req.body;
     const msg = {
-      to: "parriscodes@gmail.com",
+      to: "dispatch@landseaair-nc.com",
       from: `${email}`,
       subject: `${name.toUpperCase()} sent you a message`,
       text: `Email => ${email}`,
